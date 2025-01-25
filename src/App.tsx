30933e343a2490e9e81d50e8c704fc3ca9866570
Matias Nuniez <johndoe@example.com>
2025-01-24 23:49:45 -0300

    header and history

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,55 @@
 import React from 'react';
 import './App.css';
 import { Login } from './components/login';
-import { BrowserRouter, Routes, Route } from 'react-router';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router';
 import { Products } from './components/products';
+import { Month_end } from './components/month-end';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login/>} />
-        <Route path="/register" element={<Login/>} />
-        <Route path="/products" element={<Products/>} />
-        <Route path='/resetPassword' element={<Login/>} />
-      </Routes>
+      <MainContent />
     </BrowserRouter>
+  );
+}
+
+function MainContent() {
+  const location = useLocation();
+
+  return (
+    <>
+      {location.pathname === '/login' || location.pathname === '/resetPassword' ? null : (
+        <header className="bg-gray-800 p-4 text-white flex flex-col md:flex-row justify-between items-center">
+          <nav className="mb-4 md:mb-0">
+            <ul className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4">
+              <li>
+                <a href="/products" className="hover:underline font-semibold">
+                  Products
+                </a>
+              </li>
+              <li>
+                <a href="/history" className="hover:underline font-semibold">
+                  History
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <button
+            onClick={() => console.log('Close session')}
+            className="bg-red-500 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded"
+          >
+            Close Session
+          </button>
+        </header>
+      )}
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Login />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/resetPassword" element={<Login />} />
+        <Route path="/history" element={<Month_end />} />
+      </Routes>
+    </>
   );
 }
 
--- src/components/products.tsx
+++ src/components/products.tsx
@@ -1,15 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export const Products = () => {
+  
+  // const [data, setData] = useState<>([]);
+
+  const getProducts = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/products');
+      const data = await response.json();
+      console.log(data);
+      // setData(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    
+  }, [])  
+  
   return (
     <div className='container mx-auto px-4 sm:px-6 lg:px-8'>
       <h1 className='font-semibold m-6 text-center text-lg sm:text-xl lg:text-2xl'>Listado de productos</h1>
       <div className="flex justify-center mb-4">
         <input
           type="text"
-          placeholder="Buscar producto"
+          placeholder="Buscar producto..."
           className="p-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500 w-full sm:w-1/2 lg:w-1/3"
           // onChange={(e) => handleSearch(e.target.value)}
+        />
+      </div>
+      <div className="flex justify-center mb-4 flex-wrap sm:flex-nowrap">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded mx-2 my-1 sm:my-0">
+          Vender
+        </button>
+        <button className="bg-green-500 hover:bg-green-700 text-white font-semibold py-2 px-4 rounded mx-2 my-1 sm:my-0">
+          Cargar
+        </button>
+        <button className="bg-red-500 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded mx-2 my-1 sm:my-0">
+          Cerrar mes
+        </button>
+        <input
+          type="text"
+          placeholder="Total vendido"
+          className="p-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500 w-full sm:w-auto lg:w-auto mx-2 my-1 sm:my-0"
+          value={`$ ${0}`}
+          readOnly
         />
       </div>
       <table className="min-w-full divide-y divide-gray-200">
@@ -30,6 +66,9 @@ export const Products = () => {
         <th scope="col" className="px-2 sm:px-4 lg:px-6 py-3 text-left text-xs sm:text-sm lg:text-base font-medium text-gray-500 uppercase tracking-wider">
           Tipo
         </th>
+        <th scope="col" className="px-2 sm:px-4 lg:px-6 py-3 text-left text-xs sm:text-sm lg:text-base font-medium text-gray-500 uppercase tracking-wider">
+          Seleccionado
+        </th>
         </tr>
       </thead>
       <tbody className="bg-white divide-y divide-gray-200">
@@ -38,6 +77,8 @@ export const Products = () => {
         <td className="px-2 sm:px-4 lg:px-6 py-4 whitespace-nowrap text-sm sm:text-base lg:text-lg text-gray-500">100</td>
         <td className="px-2 sm:px-4 lg:px-6 py-4 whitespace-nowrap text-sm sm:text-base lg:text-lg text-gray-500">$10.00</td>
         <td className="px-2 sm:px-4 lg:px-6 py-4 whitespace-nowrap text-sm sm:text-base lg:text-lg text-gray-500">20</td>
+        <td className="px-2 sm:px-4 lg:px-6 py-4 whitespace-nowrap text-sm sm:text-base lg:text-lg text-gray-500">Medicamentos</td>
+        <td className="px-2 sm:px-4 lg:px-6 py-4 whitespace-nowrap text-sm sm:text-base lg:text-lg text-gray-500"><input type="checkbox" name="selectProduct" id="selectProduct"/></td>
         </tr>
       </tbody>
       </table>
